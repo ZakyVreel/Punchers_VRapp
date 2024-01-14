5ef07a7ea6d0565b3828d108e17d0060c0a08bae
zakariya.saoula <zakariya.saoula@etu.uca.fr>
2024-01-14 18:39:35 +0100

    :bug: Fix converter

--- Kinect_TP/Kinect_TP/Converter/BoolToColorConverter.cs
+++ Kinect_TP/Kinect_TP/Converter/BoolToColorConverter.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace Kinect_TP.Converter
 {
@@ -13,11 +14,14 @@ namespace Kinect_TP.Converter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if ( value is bool and true)
+            if (value is bool isActive && isActive)
             {
-                return Brushes.Green;
+                return new SolidColorBrush(Colors.Green);
             }
-            return Brushes.Red;
+            else
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
