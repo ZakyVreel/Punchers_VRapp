0c5058df56adb3c954cc2286412e95c3ab0f178e
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2024-02-10 00:41:33 +0100

    L'adversaire bouge + binding pour l'image

--- Kinect_TP/Punchers/ViewModel/PunchersVM.cs
+++ Kinect_TP/Punchers/ViewModel/PunchersVM.cs
@@ -8,7 +8,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace Punchers.ViewModel
 {
@@ -20,9 +23,20 @@ namespace Punchers.ViewModel
         public ICommand StartAcqueringFramesCommand { get; private set; }
         public ICommand StopAcqueringFramesCommand { get; private set; }
 
+        //Le timer pour l'adversaire
+        private readonly DispatcherTimer enemyTimer = new DispatcherTimer();
+        private bool movingRight = true;
+
+        private double enemyPositionX;
+
+        private string enemyImagePath = "/images/enemy-stand.png";
+        public string EnemyImagePath
+        {
+            get { return enemyImagePath; }
+            private set { SetProperty(ref enemyImagePath, value); }
+        }
+
         public Visibility StartTextVisibility { get; private set; }
-
-
 
         public PunchersVM() {
             KinectManager = new KinectManager();
@@ -31,6 +45,10 @@ namespace Punchers.ViewModel
             GestureManager.AddGestures(BoxingGestureFactory);
             StartAcqueringFramesCommand = new RelayCommand(StartAcqueringFrames);
             StopAcqueringFramesCommand = new RelayCommand(StopAcqueringFrames);
+
+            enemyTimer.Interval = TimeSpan.FromSeconds(1);
+            enemyTimer.Tick += EnemyTimer_Tick;
+            enemyTimer.Start();
 
             GestureManager.GestureRecognized += GestureManager_GestureReco;
             StartTextVisibility = Visibility.Visible;
@@ -48,6 +66,31 @@ namespace Punchers.ViewModel
             GestureManager.StopAcquiringFrame();
         }
 
+        private void EnemyTimer_Tick(object sender, EventArgs e)
+        {
+            // Déplacer l'adversaire
+            if (movingRight)
+            {
+                enemyPositionX += 10; // Ajustez la vitesse de déplacement ici
+            }
+            else
+            {
+                enemyPositionX -= 10; // Ajustez la vitesse de déplacement ici
+            }
+
+            // Changer de direction si nécessaire
+            if (enemyPositionX >= 800 - 50) // Largeur de la fenêtre - Largeur de l'adversaire
+            {
+                movingRight = false;
+            }
+            else if (enemyPositionX <= 0)
+            {
+                movingRight = true;
+            }
+
+            // Appliquez la nouvelle position
+            //EnemyImagePath = $"/images/enemy-stand.png"; // Mettez à jour le chemin de l'image de l'adversaire
+        }
 
         private void GestureManager_GestureReco(object sender, GestureRecognizedEventArgs e)
         {
