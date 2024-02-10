09ad2582563b3c7c845a5c96241e8f661f51f88a
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2024-02-10 02:27:55 +0100

    Systeme des attaque pour l'enemy fait

--- Kinect_TP/Punchers/ViewModel/PunchersVM.cs
+++ Kinect_TP/Punchers/ViewModel/PunchersVM.cs
@@ -20,6 +20,24 @@ namespace Punchers.ViewModel
         public KinectManager KinectManager { get; private set; }
         public BoxingGestureFactory BoxingGestureFactory { get; private set; }
 
+        private readonly DispatcherTimer enemyChangeTimer = new DispatcherTimer();
+        private readonly Random random = new Random();
+        private readonly string[] enemyImagePaths = { "/images/enemy-punch1.png", "/images/enemy-punch2.png", "/images/enemy-block.png" };
+
+        private int enemyLife = 100;
+        public int EnemyLife
+        {
+            get { return enemyLife; }
+            set { SetProperty(ref enemyLife, value); }
+        }
+
+        private int boxerLife = 100;
+        public int BoxerLife
+        {
+            get { return boxerLife; }
+            set { SetProperty(ref boxerLife, value); }
+        }
+
         public ICommand StartAcqueringFramesCommand { get; private set; }
         public ICommand StopAcqueringFramesCommand { get; private set; }
 
@@ -29,6 +47,13 @@ namespace Punchers.ViewModel
         {
             get { return enemyImagePath; }
             private set { SetProperty(ref enemyImagePath, value); }
+        }
+
+        private string boxerImagePath = "/images/boxer-stand.png";
+        public string BoxerImagePath
+        {
+            get { return boxerImagePath; }
+            private set { SetProperty(ref boxerImagePath, value); }
         }
 
         public Visibility StartTextVisibility { get; private set; }
@@ -41,8 +66,12 @@ namespace Punchers.ViewModel
             StartAcqueringFramesCommand = new RelayCommand(StartAcqueringFrames);
             StopAcqueringFramesCommand = new RelayCommand(StopAcqueringFrames);
 
+            enemyChangeTimer.Interval = TimeSpan.FromSeconds(4); // Changer toutes les 4 secondes
+            enemyChangeTimer.Tick += EnemyAttack_Tick;
+            enemyChangeTimer.Start();
 
             GestureManager.GestureRecognized += GestureManager_GestureReco;
+
             StartTextVisibility = Visibility.Visible;
 
         }
@@ -55,6 +84,32 @@ namespace Punchers.ViewModel
         private void StopAcqueringFrames()
         {
             GestureManager.StopAcquiringFrame();
+        }
+
+        private void EnemyAttack_Tick(object sender, EventArgs e)
+        {
+            // Changer le chemin de l'image de l'adversaire aléatoirement
+            int index = random.Next(enemyImagePaths.Length);
+            EnemyImagePath = enemyImagePaths[index];
+
+            // Vérifier si l'image du boxeur est "boxer_block"
+            if (BoxerImagePath.Contains("boxer-block"))
+            {
+                // Ne déduire aucun point de vie du boxeur
+            }
+            else
+            {
+                // Si l'image de l'ennemi est un coup de poing, déduire 20 points de vie du boxerLife
+                if (EnemyImagePath.Contains("enemy-punch"))
+                {
+                    BoxerLife -= 25;
+                }
+            }
+
+            // Réinitialiser l'image de l'adversaire après un certain délai
+            Task.Delay(500).ContinueWith(_ => {
+                EnemyImagePath = "/images/enemy-stand.png";
+            });
         }
 
 
